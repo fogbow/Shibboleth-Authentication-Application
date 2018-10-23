51dd5d3f54e24ad91b5ed4549ae266e1e9c424f1
Francisco Germano <fgan1@hotmail.com>
2018-10-23 16:17:52 -0300

    Update start-shib-app.sh

--- bin/start-shib-app.sh
+++ bin/start-shib-app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIRNAME=`dirname $0`
 cd $DIRNAME/..
-java -Dlog4j.configuration=file:log4j.properties -cp target/shib-app-0.0.1-SNAPSHOT-jar-with-dependencies.jar:. org/fogbowcloud/cafe/Main shib-app.conf > /dev/null &
+java -Dlog4j.configuration=file:log4j.properties -cp target/shib-app-0.0.1-SNAPSHOT-jar-with-dependencies.jar:. org/fogbowcloud/shipapp/Main shib-app.conf > /dev/null &
