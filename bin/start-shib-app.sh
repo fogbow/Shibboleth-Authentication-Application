29d1b746c7e69ac083a0cb1fc47dea1ff437f4d8
fg4n <fgan1@hotmail.com>
2019-07-04 17:16:13 -0300

    Fixing package name in the start-shib-app.sh

--- bin/start-shib-app.sh
+++ bin/start-shib-app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIRNAME=`dirname $0`
 cd $DIRNAME/..
-java -Dlog4j.configuration=file:log4j.properties -cp target/shib-app-0.0.1-SNAPSHOT-jar-with-dependencies.jar:. cloud/fogbow/shipapp/Main shibboleth-authentication-application.conf > /dev/null &
+java -Dlog4j.configuration=file:log4j.properties -cp target/shib-app-0.0.1-SNAPSHOT-jar-with-dependencies.jar:. cloud/fogbow/shibapp/Main shibboleth-authentication-application.conf > /dev/null &
